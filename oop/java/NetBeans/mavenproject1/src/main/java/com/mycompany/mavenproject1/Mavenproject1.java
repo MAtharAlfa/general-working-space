bade4e20caa4abe0e04d4281ecaf3dd6e8af7f06
MAtharAlfa <alfaathar1311@gmail.com>
2026-09-02 14:07:24 +0700

    feat: NetBeans setup

---
+++ oop/java/NetBeans/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
